7185dfa1e68ba65bff5711bc5874cd4eaf145dde
Brian Chenault <brianchenault@WTA-MBP44-Brian.local>
2013-06-26 17:38:26 -0400

    further updating

--- public/js/app.js
+++ public/js/app.js
@@ -1,71 +1,75 @@
 'use strict';
 
-var watlunch = angular.module('WatLunch', ['firebase']),
-    WatLunch = {};
-WatLunch.username = '';
-WatLunch.thedate = moment().format("YYYYMMDD");
+(function() {
 
-var firebase = new Firebase('https://watlunch.firebaseIO.com/'),
-    authClient = new FirebaseAuthClient(firebase, function(error, user) {
-        if (error) {
-            // an 0error occurred while attempting login
-            console.log(error);
-        } else if (user) {
-            // user authenticated with Firebase
-            //console.log(user);
-            $('.auth .in').hide();
-            $('.auth .out, .right').show();
-            window.WatLunch.username = user.displayName !== '' ? user.displayName : user.username;
-            $('.welcome').html('Welcome, ' + window.WatLunch.username);
-        } else {
-            // user is logged out
-            $('.auth .out, .right').hide();
-        }
-    });
+    var watlunch = angular.module('WatLunch', ['firebase']),
+        WatLunch = {
+            baseurl: 'https://watlunch.firebaseIO.com/',
+            thedate: moment().format("YYYYMMDD"),
+            username: localStorage['watlunch_username'] ?
+                localStorage['watlunch_username'] : ''
+        };
 
-$('.login.btn-github').click(function() {
-    authClient.login('github', {
-        rememberMe: true,
-        scope: 'user'
-    });
-});
-
-$('.login.btn-facebook').click(function() {
-    authClient.login('facebook', {
-        rememberMe: true,
-        scope: 'email'
-    });
-});
-
-$('.login.btn-twitter').click(function() {
-    authClient.login('twitter', {
-        rememberMe: true
-    });
-});
-
-$('.out a').click(function() {
-    authClient.logout();
-    $('.auth .in').show();
-    $('.auth .out, .right').hide();
-    $('.welcome').html('');
-});
-
-angular.module('WatLunch', ['firebase'])
-    .controller('watlunch', ['$scope', '$timeout', 'angularFireCollection',
-        function($scope, $timeout, angularFireCollection) {
-            var url = 'https://watlunch.firebaseIO.com/restaurants';
-            $scope.restaurants = angularFireCollection(new Firebase(url).limit(50));
-
-            $scope.addRestaurant = function() {
-                $scope.restaurants.add({name: $scope.restaurant});
+    var authClient = new FirebaseAuthClient(new Firebase(WatLunch.baseurl),
+        function(error, user) {
+            if (error) {
+                // an error occurred while attempting login
+                console.log(error);
+            } else if (user) {
+                // user authenticated with Firebase
+                $('.auth .in').hide();
+                $('.auth .out, .main').show();
+                WatLunch.username = user.displayName !== '' ? user.displayName : user.username;
+                localStorage['watlunch_username'] = WatLunch.username;
+                $('.welcome').html('Welcome, ' + WatLunch.username);
+            } else {
+                // user is logged out
+                $('.auth .out, .main').hide();
             }
-            $scope.vote = function(restaurant) {
-                var url = 'https://watlunch.firebaseIO.com/restaurants/' + restaurant.$id + '/votes/' + WatLunch.thedate ;
-                restaurant.votes = angularFireCollection(new Firebase(url).limit(50));
-                restaurant.votes.add({name: window.WatLunch.username});
-                console.log(restaurant.votes);
+        });
 
-                $('.vote').hide();
+    $('.login.btn-github').click(function() {
+        authClient.login('github', {
+            rememberMe: true,
+            scope: 'user'
+        });
+    });
+
+    $('.login.btn-facebook').click(function() {
+        authClient.login('facebook', {
+            rememberMe: true,
+            scope: 'email'
+        });
+    });
+
+    $('.login.btn-twitter').click(function() {
+        authClient.login('twitter', {
+            rememberMe: true
+        });
+    });
+
+    $('.out a').click(function() {
+        authClient.logout();
+        $('.auth .in').show();
+        $('.auth .out, .right').hide();
+        $('.welcome').html('');
+    });
+
+    angular.module('WatLunch', ['firebase'])
+        .controller('watlunch', ['$scope', '$timeout', 'angularFireCollection',
+            function($scope, $timeout, angularFireCollection) {
+                var url = WatLunch.baseurl + 'restaurants';
+                $scope.restaurants = angularFireCollection(new Firebase(url).limit(50));
+
+                $scope.addRestaurant = function() {
+                    $scope.restaurants.add({name: $scope.restaurant});
+                }
+                $scope.vote = function(restaurant) {
+                    var url = WatLunch.baseurl + '/restaurants/' + restaurant.$id + '/votes/' + WatLunch.thedate ;
+                    restaurant.votes = angularFireCollection(new Firebase(url).limit(50));
+                    restaurant.votes.add({name: WatLunch.username});
+                    $('.vote').remove();
+                }
             }
-        }
-    ]);
+        ]);
+})();
