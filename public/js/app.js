3ced7d0e4d0eb14b0160665dc418fc3a5579c5fa
Brian Chenault <brianchenault@WTA-MBP44-Brian.local>
2013-06-28 14:05:44 -0400

    more UI updates

--- public/js/app.js
+++ public/js/app.js
@@ -10,63 +10,73 @@
                 localStorage['watlunch_username'] : ''
         };
 
-    WatLunch.authClient = new FirebaseAuthClient(new Firebase(WatLunch.baseurl),
-        function(error, user) {
-            if (error) {
-                // an error occurred while attempting login
-                console.log(error);
-            } else if (user) {
-                // user authenticated with Firebase
-                $('.in').hide();
-                $('.auth .out, .main').show();
-                WatLunch.username = user.displayName !== '' ? user.displayName : user.username;
-                localStorage['watlunch_username'] = WatLunch.username;
-                $('.welcome').html('Welcome, ' + WatLunch.username);
-            } else {
-                // user is logged out
-                $('.auth .out, .main').hide();
-            }
-        });
-
-    $('.out a').click(function() {
-        WatLunch.authClient.logout();
-        $('.in').show();
-        $('.auth .out, .right').hide();
-        $('.welcome').html('');
-    });
-
     angular.module('WatLunch', ['firebase'])
         .controller('watlunch', ['$scope', '$timeout', 'angularFireCollection',
             function($scope, $timeout, angularFireCollection) {
                 var url = WatLunch.baseurl + 'restaurants';
                 $scope.restaurants = angularFireCollection(new Firebase(url).limit(50));
+                var userVoted = false;
 
                 $scope.addRestaurant = function() {
                     $scope.restaurants.add({name: $scope.restaurant});
                 }
+
                 $scope.vote = function(restaurant) {
                     var url = WatLunch.baseurl + '/restaurants/' + restaurant.$id + '/votes/' + WatLunch.thedate ;
                     restaurant.votes = angularFireCollection(new Firebase(url).limit(50));
                     restaurant.votes.add({name: WatLunch.username});
-                    $('.vote').remove();
+                    //$('.vote').remove();
+                    userVoted = true;
                 }
+
                 $scope.githubLogin = function() {
                     WatLunch.authClient.login('github', {
                         rememberMe: true,
                         scope: 'user'
                     });
                 }
+
                 $scope.facebookLogin = function() {
                     WatLunch.authClient.login('facebook', {
                         rememberMe: true,
                         scope: 'email'
                     });
                 }
+
                 $scope.twitterLogin = function() {
                     WatLunch.authClient.login('twitter', {
                         rememberMe: true
                     });
                 }
-            }
+
+                $scope.logout = function() {
+                    WatLunch.authClient.logout();
+                    $('.auth .out, .right').hide();
+                    $('.welcome').html('');
+                };
+
+                $scope.userHasVoted = function() {
+                    return userVoted;
+                };
+
+                WatLunch.authClient = new FirebaseAuthClient(new Firebase(WatLunch.baseurl),
+                    function(error, user) {
+                        $scope.user = user;
+
+                        if (error) {
+                            // an error occurred while attempting login
+                            console.log(error);
+                        } else if (user) {
+                            // user authenticated with Firebase
+                            $('.auth .out, .main').show();
+                            WatLunch.username = user.displayName !== '' ? user.displayName : user.username;
+                            localStorage['watlunch_username'] = WatLunch.username;
+                            $('.welcome').html('Welcome, ' + WatLunch.username);
+                        } else {
+                            // user is logged out
+                            $('.auth .out, .main').hide();
+                        }
+                    });
+                }
         ]);
 })();
